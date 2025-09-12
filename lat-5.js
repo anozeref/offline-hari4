2df37a85d74563b1ee7fed22e569497e586425b6
anozeref <kantorposfikry@gmail.com>
2025-09-12 15:36:03 +0700

    Pemilu Ketua Kelas

--- lat-5.js
+++ lat-5.js
@@ -1,7 +1,7 @@
 //Splice
 var arr=['ade','asep', 'agus'];
 arr.splice(2,0,'denis');
-console.log (arr.join(' - '));
 //Slice
+console.log (arr.join(' - '));
 var arr1=arr.slice(0,3);
 console.log (arr1.join(' - '));
